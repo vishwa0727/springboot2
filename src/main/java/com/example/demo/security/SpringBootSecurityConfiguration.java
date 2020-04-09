8a88b0e6e57b945da5994552923e4975d5c6a5f0
vishwa0727 <63392928+vishwa0727@users.noreply.github.com>
2020-04-09 14:07:45 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/security/SpringBootSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringBootSecurityConfiguration extends WebSecurityConfigurerAdapter  {
+	
+	@Override public void configure(HttpSecurity http) throws Exception {
+        /*http
+         .authorizeRequests()
+        .antMatchers("/**").hasAnyRole("USER")
+        .and()
+        .formLogin().loginPage("/login").permitAll();*/
+        http.csrf().disable();
+    }
+
+}
